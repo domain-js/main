29d2b4ced86c4089bd3b863279390a27cd425724
Redstone Zhao <13740080@qq.com>
2021-11-20 02:25:31 +0800

    reconstruct counter with typescript and add unit test cases coverage 100%

---
+++ src/deps/counter/__test__/index.test.ts
@@ -0,0 +1,57 @@
+import { Main as Counter } from "..";
+
+const redis = {
+  hget: jest.fn(),
+  hset: jest.fn(),
+  hincrby: jest.fn(),
+};
+describe("counter", () => {
+  const counter = Counter({ counter: { key: "test-key" } }, { redis });
+
+  describe("get", () => {
+    it("case1", async () => {
+      expect(await counter.get("t1")).toBe(0);
+      expect(redis.hget.mock.calls.length).toBe(1);
+      expect(redis.hget.mock.calls.pop()).toEqual(["test-key", "t1"]);
+    });
+
+    it("case2", async () => {
+      redis.hget.mockResolvedValueOnce(200);
+      expect(await counter.get("t1")).toBe(200);
+      expect(redis.hget.mock.calls.length).toBe(1);
+      expect(redis.hget.mock.calls.pop()).toEqual(["test-key", "t1"]);
+    });
+  });
+
+  describe("set", () => {
+    it("case1", async () => {
+      redis.hset.mockResolvedValueOnce(1);
+      expect(await counter.set("t2", 201)).toBe(1);
+      expect(redis.hset.mock.calls.length).toBe(1);
+      expect(redis.hset.mock.calls.pop()).toEqual(["test-key", "t2", 201]);
+    });
+
+    it("case2", async () => {
+      redis.hset.mockRejectedValueOnce(Error("write failed"));
+      expect(() => counter.set("t2", 201)).rejects.toThrow("write failed");
+      expect(redis.hset.mock.calls.length).toBe(1);
+      expect(redis.hset.mock.calls.pop()).toEqual(["test-key", "t2", 201]);
+    });
+  });
+
+  describe("incr", () => {
+    it("case1", async () => {
+      redis.hincrby.mockResolvedValueOnce(1);
+      expect(await counter.incr("t3")).toBe(1);
+      expect(redis.hincrby.mock.calls.length).toBe(1);
+      expect(redis.hincrby.mock.calls.pop()).toEqual(["test-key", "t3", 1]);
+    });
+
+    it("case2", async () => {
+      redis.hincrby.mockRejectedValueOnce(Error("hincrby error"));
+      expect(() => counter.incr("t3")).rejects.toThrow("hincrby error");
+      expect(redis.hincrby.mock.calls.length).toBe(1);
+      expect(redis.hincrby.mock.calls.pop()).toEqual(["test-key", "t3", 1]);
+    });
+  });
+});
---
+++ src/deps/counter/index.ts
@@ -0,0 +1,37 @@
+import { Redis } from "ioredis";
+
+interface Cnf {
+  counter: {
+    key: string;
+  };
+}
+
+interface Deps {
+  redis: {
+    hget: Redis["hget"];
+    hset: Redis["hset"];
+    hincrby: Redis["hincrby"];
+  };
+}
+
+export function Main(cnf: Cnf, deps: Deps) {
+  const {
+    counter: { key: REDIS_KEY },
+  } = cnf;
+
+  const { redis } = deps;
+
+  const get = async (key: string) => {
+    const num = await redis.hget(REDIS_KEY, key);
+
+    return Number(num) | 0;
+  };
+
+  const set = (key: string, val: number) => redis.hset(REDIS_KEY, key, Math.max(0, val | 0));
+
+  const incr = (key: string) => redis.hincrby(REDIS_KEY, key, 1);
+
+  return { get, set, incr };
+}
+
+export const Deps = ["redis"];
