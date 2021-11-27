7034511a61e2b04f6e6d78d79f9427f1502215ac
Redstone Zhao <13740080@qq.com>
2021-11-27 11:13:33 +0800

    Improve the type annotation and unit test cases of singer module

---
+++ src/deps/signer/__test__/index.test.ts
@@ -0,0 +1,41 @@
+import { Main } from "..";
+
+jest.spyOn(Date, "now");
+
+describe("Singer", () => {
+  const signer = Main();
+  describe("generator", () => {
+    it("case1", () => {
+      const signature = signer.generator(
+        {
+          uri: "/users",
+          key: "test key",
+          method: "",
+          signMethod: "HmacSHA256",
+          signVersion: "1",
+          timestamp: 337008447,
+        },
+        "secret key",
+      );
+
+      expect(signature).toBe("0iZkHO/G+ZXYFmyYBLjoxyqQYGTx20TqlaBTfk4BHmI=");
+      expect(typeof signature).toBe("string");
+    });
+  });
+
+  describe("request", () => {
+    it("case1", () => {
+      (Date.now as any).mockReturnValueOnce(337008447 * 1000);
+      const info = signer.request("/users", "", "test key", "secret key");
+      expect(typeof info).toBe("object");
+      expect(info).toEqual({
+        "x-auth-signature": "0iZkHO/G+ZXYFmyYBLjoxyqQYGTx20TqlaBTfk4BHmI=",
+        "x-auth-key": "test key",
+        "x-auth-method": "",
+        "x-auth-timestamp": 337008447,
+        "x-auth-sign-method": "HmacSHA256",
+        "x-auth-sign-version": "1",
+      });
+    });
+  });
+});
--- src/deps/signer/index.ts
+++ src/deps/signer/index.ts
@@ -1,15 +1,32 @@
 import * as crypto from "crypto";
 
+/** Objects to be encrypted */
 export interface Opt {
+  /** The URI does not contain the root path part */
   uri: string;
+  /** Key for signature calculation */
   key: string;
+  /** Second timestamp */
   timestamp: number;
+  /** Signature algorithm, fixed as hmacsha256 */
   signMethod: "HmacSHA256";
+  /** Signature version, fixed as 1 */
   signVersion: "1";
+  /** Method name of the request interface, internal domain method name, not HTTP verb */
   method: string;
 }
 
+/**
+ * API interface encryption signature algorithm module, based on sha256
+ * @returns generator And request Methods
+ */
 export function Main() {
+  /**
+   * Core encryption algorithm, the result is returned as Base64 string
+   * @param opt Objects to be encrypted
+   * @param secret Calculate the private key of the signature
+   * @returns The result of the signature is in the format of Base64 string
+   */
   const generator = (opt: Opt, secret: string) => {
     const string = Object.keys(opt)
       .map((k) => `${k}=${encodeURIComponent(opt[k as keyof Opt])}`)
@@ -19,6 +36,14 @@ export function Main() {
     return h.update(string).digest("base64");
   };
 
+  /**
+   * Get all the signature encryption information required for a request
+   * @param uri The URI does not contain the root path part
+   * @param method Method name of the request interface, internal domain method name, not HTTP verb
+   * @param key Key for signature calculation
+   * @param secret Calculate the private key of the signature
+   * @returns
+   */
   const request = (uri: string, method: string, key: string, secret: string) => {
     const opt: Opt = {
       uri,
@@ -38,7 +63,7 @@ export function Main() {
       "x-auth-timestamp": opt.timestamp,
       "x-auth-sign-method": opt.signMethod,
       "x-auth-sign-version": opt.signVersion,
-    };
+    } as const;
   };
 
   return { generator, request };
