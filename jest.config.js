8ebba5e7118c712ababe2327e81639f5423d80fd
Redstone Zhao <13740080@qq.com>
2021-11-29 16:24:41 +0800

    modify jest coverage rule, ignore samples files

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   forceExit: true,
+  collectCoverageFrom: [
+    "src/utils/*.ts",
+    "src/deps/**/*.ts",
+    "src/dm/index.ts",
+    "src/http/**/*.ts",
+    "src/cli/*.ts",
+  ],
 };
