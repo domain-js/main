d7eec7c3a63c1eedf9d8ef2fdfe0e723324394d2
Redstone Zhao <13740080@qq.com>
2021-12-02 12:21:21 +0800

    Improve code writing to avoid missing comments

--- src/deps/signer/index.ts
+++ src/deps/signer/index.ts
@@ -22,51 +22,56 @@ export interface Opt {
  */
 export function Main() {
   /**
-   * Core encryption algorithm, the result is returned as Base64 string
-   * @param opt Objects to be encrypted
-   * @param secret Calculate the private key of the signature
-   * @returns The result of the signature is in the format of Base64 string
+   * API interface encryption signature algorithm module, based on sha256
    */
-  const generator = (opt: Opt, secret: string) => {
-    const string = Object.keys(opt)
-      .map((k) => `${k}=${encodeURIComponent(opt[k as keyof Opt])}`)
-      .sort()
-      .join("&");
-    const h = crypto.createHmac("sha256", secret);
-    return h.update(string).digest("base64");
+  const returns = {
+    /**
+     * Core encryption algorithm, the result is returned as Base64 string
+     * @param opt Objects to be encrypted
+     * @param secret Calculate the private key of the signature
+     * @returns The result of the signature is in the format of Base64 string
+     */
+    generator(opt: Opt, secret: string) {
+      const string = Object.keys(opt)
+        .map((k) => `${k}=${encodeURIComponent(opt[k as keyof Opt])}`)
+        .sort()
+        .join("&");
+      const h = crypto.createHmac("sha256", secret);
+      return h.update(string).digest("base64");
+    },
+
+    /**
+     * Get all the signature encryption information required for a request
+     * @param uri The URI does not contain the root path part
+     * @param method Method name of the request interface, internal domain method name not HTTP verb
+     * @param key Key for signature calculation
+     * @param secret Calculate the private key of the signature
+     * @returns
+     */
+    request(uri: string, method: string, key: string, secret: string) {
+      const opt: Opt = {
+        uri,
+        key,
+        timestamp: (Date.now() / 1000) | 0,
+        signMethod: "HmacSHA256",
+        signVersion: "1",
+        method,
+      };
+
+      const signature = returns.generator(opt, secret);
+
+      return {
+        "x-auth-signature": signature,
+        "x-auth-key": key,
+        "x-auth-method": method,
+        "x-auth-timestamp": opt.timestamp,
+        "x-auth-sign-method": opt.signMethod,
+        "x-auth-sign-version": opt.signVersion,
+      } as const;
+    },
   };
 
-  /**
-   * Get all the signature encryption information required for a request
-   * @param uri The URI does not contain the root path part
-   * @param method Method name of the request interface, internal domain method name, not HTTP verb
-   * @param key Key for signature calculation
-   * @param secret Calculate the private key of the signature
-   * @returns
-   */
-  const request = (uri: string, method: string, key: string, secret: string) => {
-    const opt: Opt = {
-      uri,
-      key,
-      timestamp: (Date.now() / 1000) | 0,
-      signMethod: "HmacSHA256",
-      signVersion: "1",
-      method,
-    };
-
-    const signature = generator(opt, secret);
-
-    return {
-      "x-auth-signature": signature,
-      "x-auth-key": key,
-      "x-auth-method": method,
-      "x-auth-timestamp": opt.timestamp,
-      "x-auth-sign-method": opt.signMethod,
-      "x-auth-sign-version": opt.signVersion,
-    } as const;
-  };
-
-  return { generator, request };
+  return returns;
 }
 
 export const Deps = [];
