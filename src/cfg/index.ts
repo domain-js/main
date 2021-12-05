117f830d0a0292e668e59ec1d2188c630b34f01e
Redstone Zhao <13740080@qq.com>
2021-12-05 20:04:45 +0800

    Add cfg module to manage config item info

---
+++ src/cfg/index.ts
@@ -0,0 +1,28 @@
+import * as _ from "lodash";
+import Ajv, { Schema } from "ajv";
+import addFormats from "ajv-formats";
+
+interface Cnf {
+  [propName: string]: string | undefined;
+}
+
+export function Main(object: Cnf, schema: Schema) {
+  if (typeof schema !== "object") throw Error("object type isnt an object");
+  const FIELDS = new Set(Object.keys(schema.properties));
+  const ajv = new Ajv({ allowUnionTypes: true, coerceTypes: true, useDefaults: true });
+  addFormats(ajv);
+  const validate = ajv.compile(schema);
+
+  const obj = _.pick(object, [...FIELDS]);
+  if (!validate(obj)) {
+    console.log("Config object data validate failed", obj);
+    console.log(JSON.stringify(validate.errors, null, 2));
+    throw Error("Config object data has error");
+  }
+
+  return (key: string) => {
+    if (!FIELDS.has(key)) throw Error(`Key: ${key} 未提前在 schema 中定义, 请先定义`);
+
+    return obj[key];
+  };
+}
