47e0f5a4b0e9851a5a3ce5c762be66f9b0d2f7eb
Redstone Zhao <13740080@qq.com>
2022-05-27 23:45:42 +0000

    feat: 增加控频模块,用来控制某些操作的一段时间内的最大次数

--- src/deps/frequency/index.ts
+++ src/deps/frequency/index.ts
@@ -18,6 +18,12 @@ export function Main(cnf: Cnf, deps: Deps) {
     frequency: { key: KEY },
   } = cnf;
 
+  /**
+   * 检测频次控制，超过显示过抛出异常
+   * @param filed 控频字段key
+   * @param ms 控频周期长度 毫秒
+   * @param limit 控频极限次数
+   */
   const check = async (filed: string, ms: number, limit: number) => {
     const now = Date.now();
     const t = Math.floor(now / ms);
